70172b59f39494cd0cc1577b7b3651bc9348d938
Zubin Shah <www.zubinshah1886@gmail.com>
2022-12-27 17:08:23 +0530

    first commit

---
+++ src/universitymanagementsystem/login.java
@@ -0,0 +1,71 @@
+package universitymanagementsystem;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class login extends JFrame implements ActionListener{
+
+    JButton login, Cancel;
+    login(){
+        setTitle("Admin Login");
+        getContentPane().setBackground(Color.LIGHT_GRAY);
+        setLayout(null);
+
+        JLabel lblUsername = new JLabel("Username");
+        add(lblUsername);
+        lblUsername.setBounds(40,30,100,20);
+
+        JTextField tfUsername = new JTextField();
+        tfUsername.setBounds(150,30,140,20);
+        add(tfUsername);
+        
+        JLabel lblPassword = new JLabel("Password");
+        add(lblPassword);
+        lblPassword.setBounds(40,60,100,20);
+
+        JPasswordField tfPassword = new JPasswordField();
+        tfPassword.setBounds(150,60,140,20);
+        add(tfPassword);
+
+        login = new JButton("Login");
+        login.setBounds(75,105,80,30);
+        login.setBackground(Color.BLACK);
+        login.setForeground(Color.WHITE);
+        login.setFont(new Font("Tahoma",Font.BOLD,12));
+        login.addActionListener(this);
+        add(login);
+        
+        Cancel = new JButton("Cancel");
+        Cancel.setBounds(175,105,80,30);
+        Cancel.setBackground(Color.BLACK);
+        Cancel.setForeground(Color.WHITE);
+        Cancel.setFont(new Font("Tahoma",Font.BOLD,12));
+        Cancel.addActionListener(this);
+        add(Cancel);
+
+        ImageIcon loginImage = new ImageIcon(ClassLoader.getSystemResource("icons/loginIcon.png"));
+        Image logimage = loginImage.getImage().getScaledInstance(120, 120, Image.SCALE_DEFAULT);
+        loginImage = new ImageIcon((logimage));
+        JLabel icon = new JLabel(loginImage);
+        icon.setBounds(320,20,120,120);
+        add(icon);
+
+        setSize(480,200);
+        setLocation(436,286);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public void actionPerformed(ActionEvent CallingObj){
+        if(CallingObj.getSource() == login){
+
+        }
+        else if (CallingObj.getSource() == Cancel){
+            dispose();
+        }
+    }
+
+    public static void main(String []args){
+        new login();
+    }
+}
---
+++ src/universitymanagementsystem/splash.java
@@ -0,0 +1,47 @@
+package universitymanagementsystem;
+import java.awt.Image;
+import javax.swing.*;
+
+public class splash extends JFrame implements Runnable{
+    Thread t;
+    
+    splash(){
+        t = new Thread(this);
+        t.start();
+        setTitle("Welcome");
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/third.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(800, 450, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        add(image);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // exits the program
+        
+        for(int i=0, x=0;i<300;i+=5, x+=7.5){
+            setLocation(600 - i,450 - i); // max 1240,720
+            // if(2*i < 150) setSize(2*i+200,2*i);
+            // else setSize(2*i+200,2*i-150);
+            setSize(2*i+200,x);
+            try{
+                Thread.sleep(10);
+            }
+            catch(Exception e){
+                
+            }
+        }
+    }
+    public void run(){
+        try{
+            Thread.sleep(5000);
+            setVisible(false);
+            dispose(); // exits the JFrame
+            new login();
+        }catch(Exception e){
+            
+        }
+    }
+    public static void main(String []args){
+        new splash();
+        System.out.print("Hello there!");
+    }
+}
