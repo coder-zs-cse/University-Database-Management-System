c1836678228030ce6c95040d51c602e2733cc122
Zubin Shah <www.zubinshah1886@gmail.com>
2023-02-01 15:14:27 +0530

    changed cover page

--- src/universitymanagementsystem/splash.java
+++ src/universitymanagementsystem/splash.java
@@ -9,8 +9,8 @@ public class splash extends JFrame implements Runnable{
         t = new Thread(this);
         t.start();
         setTitle("Welcome");
-        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/third.jpg"));
-        Image i2 = i1.getImage().getScaledInstance(800, 450, Image.SCALE_DEFAULT);
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/covernitgoa.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(957, 500, Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
         JLabel image = new JLabel(i3);
         add(image);
